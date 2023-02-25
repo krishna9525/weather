4b25c4b0780146ebc53368eebbf0fd6311f3d2a4
MasaiSchoo <30krishnakumar111998@gmail.com>
2023-02-25 08:50:02 +0530

    weather App

---
+++ weather/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Tempapp from './Components/Tempapp';
+
+function App() {
+  return (
+    <div className="App">
+      <Tempapp/>
+     
+    </div>
+  );
+}
+
+export default App;
